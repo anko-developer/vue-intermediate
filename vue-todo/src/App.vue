b29a1e6e4e5ff056be86ff50901bbeb091eeff24
김명욱 <anko@gsitm.com>
2022-05-26 21:24:15 +0900

    vue todo start

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+import TodoFooter from './components/TodoFooter';
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  font-family: 'Ubuntu', sans-serif;
+  text-align: center;
+  background-color: #f6f6f6;
+}
+
+input {
+  width: 200px;
+  border-style: groove;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0,0,0,.03);
+}
+</style>
